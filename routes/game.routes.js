c81587f32e7df692e4040281f27848f858a51f38
Diana Pirlici <pirlicid@gmail.com>
2022-11-05 16:49:05 +0000

    the BRACES HTML!!!!!1 wtf

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 const Game = require('../models/Game.model');
 const getGames = require('../services/api.service');
+const getOneGame = require('../services/getOneGame');
 
 
+
+router.get("/details/:id", async(req, res, next) =>{
+    try {
+        const gameId = req.params.id;
+        const game = await getOneGame(gameId);
+        const description = game.description;
+        /* console.log(description)
+        const bestDescription = description.substring(3, description.length - 3) */
+        res.render("games/game-details", game);
+    } catch (error) {
+       console.log(error); 
+    }
+    
+})
 
 
 router.post("/search", async (req, res, next) => {
