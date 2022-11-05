cb2be74c6893018bf36aa535dbd818cf13119c1b
Pedro Pedrosa <prpedrosa95@gmail.com>
2022-11-05 12:41:08 +0000

    adds api search function

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const axios = require("axios")
+//getGames is a function for searching games
+const getGames = require('../services/api.service');
+
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  /* try {
+    const responseApi = await getGames("grand", "shooter", "pc");
+    const games = responseApi.results
+    games.forEach(el => {
+      gameTitle = el.name;
+      genre = el.genres
+      platform = el.platforms
+      console.log(gameTitle, genre, platform);  
+    });
+    
+    
+  } catch (error) {
+    console.log(error);
+    next(error)
+  } */
+
   res.render("index");
 });
 
