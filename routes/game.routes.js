d6a0cf3114e362399ab5f59028c01f27fc82df08
Pedro Pedrosa <prpedrosa95@gmail.com>
2022-11-05 15:21:45 +0000

    adds search form and results

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
 const Game = require('../models/Game.model');
+const getGames = require('../services/api.service');
+
+
+
+
+router.post("/search", async (req, res, next) => {
+    const searchTerm = req.body.game;
+    try {
+        const apiResponse = await getGames(searchTerm);
+        const games = apiResponse.results
+   
+        res.render("games/game-list", {games});
+    } catch (error) {
+        console.log(error)
+        next(error);
+    }
+})
+
+
 
 module.exports = router;
+
