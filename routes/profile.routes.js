c0f08db2905f7cfea4df449c4a31830938530cb8
Pedro Pedrosa <prpedrosa95@gmail.com>
2022-11-05 18:38:55 +0000

    does some logic

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,20 @@
 const router = require("express").Router();
 const User = require('../models/User.model');
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+router.get('/profile', isLoggedIn, async (req, res, next) =>{
+    try {
+        
+        const currentUser = req.session.currentUser
+        console.log(currentUser)
+        res.render('profile/edit-profile', currentUser)
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+})
 
 module.exports = router;
 
-router.get('/profile', async (req, res, next) =>{
-    try {
-        res.render('profile/edit-profile')
-    } catch (error) {
-        console.log(error);
-    }
-})
 
