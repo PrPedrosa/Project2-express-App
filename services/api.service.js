fe57d9df4bb027ac734fc1c403c0ad9f1764757a
Pedro Pedrosa <prpedrosa95@gmail.com>
2022-11-07 23:15:02 +0000

    adds game list button logic

--- services/api.service.js
+++ services/api.service.js
@@ -12,10 +12,15 @@ const axios = require("axios");
     }
 }
  */
+
 let allGenres = "action,adventure,shooter,strategy,indie,role-playing-games-rpg,casual,simulation,puzzle,arcade,platformer,racing,massively-multiplayer,sports,fighting,family,board-games,educational,card"
 
 
 const searchGames=(gameName, page = 1)=> {
+
+    if(!gameName) gameName = ""
+    if(page === 0) page = 1
+
     let getAllGames = {
       method: "GET",
       url: `https://api.rawg.io/api/games?key=91126f5a9acd457aa8ad4ce73cd3a59f&search=${gameName}&search_precise=true&page_size=9&page=${page}`,
@@ -25,6 +30,7 @@ const searchGames=(gameName, page = 1)=> {
     };
     return axios.request(getAllGames).then(function (response) {
       const games = response.data;
+      /* console.log(response) */
       return {games, page, gameName};
     })
     .catch(error => console.log(error))
